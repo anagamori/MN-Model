748df1acd88456edf8a5031e44d4b6ca2168e94a
Akira <nagamori@usc.edu>
2019-08-13 09:45:47 -0700

    update

---
+++ Ca_test.m
@@ -0,0 +1,25 @@
+
+I_Ca = -g_Ca_N*m_N_s_vec.^2.*h_N_s_vec.*(V_s-V_Ca);
+
+f = 1;
+alpha = 0.009*1000;
+k_Ca = 2*1000;
+Ca = 0;
+
+K_d = 0.2; %(microM)
+g_K_Ca = 5; %(mS/cm^2)
+
+Ca_vec = zeros(1,length(time));
+for t = 1:length(time)
+    dCa = f*(-alpha*I_Ca(t)-k_Ca*Ca);
+    Ca = Ca+dCa/Fs;
+    Ca_vec(t) = Ca*0.01;
+end
+
+I_K_Ca = g_K_Ca*(Ca_vec./(Ca_vec+K_d)).*(V_s-V_K);
+
+figure(1)
+plot(time,Ca_vec)
+
+figure(2)
+plot(time,I_K_Ca)
--- MN_Model_Destexhe2001.m
+++ MN_Model_Destexhe2001.m
@@ -11,35 +11,10 @@ clear all
 clc
 
 %%
-Fs = 10000;
-time = 0:1/Fs:5;
+Fs = 50000;
+time = 0:1/Fs:2;
 
-%% modle parameter
-R_i = 70*10^-3; %sytoplasm resistivity (ohm*cm)
-r_s = 77.5*10^-4; % soma diameter (micrometer)
-l_s = 77.5*10^-4; %soma length (micometer)
-R_m_s = 1.15; % soma specific resistance (kohm*cm^2)
-r_d = 41.5*10^-4; % soma diameter (micrometer)
-l_d = 5519*10^-4; %soma length (micometer)
-R_m_d = 14.4; % dendrite specific resistance (kohm*cm^2)
 
-C_m  = 1*10^-3; % specific membrain capacitance (microF/cm^2)
-E_Na = 115;
-E_K = -15;
-E_l = -80;
-
-g_l = 0.045; %(mS/cm^2)
-g_Na_max = 30; %*2*pi*r_s*l_s; %(mS/cm^2)
-g_Kf_max = 4;% *2*pi*r_s*l_s; %(mS/cm^2)
-g_Ks_max = 16; %*2*pi*r_s*l_s; %(mS/cm^2)
-
-g_c = 2/(R_i*l_d/(pi*r_d^2)+R_i*l_s/(pi*r_s^2)); % coupling conductance
-g_ld = 2*pi*r_d*l_d/(R_m_d); % leak conductance for dendrite
-g_ls = 2*pi*r_s*l_s/(R_m_s); % leak conductance for soma
-C_d = 2*pi*r_d*l_d*C_m*10^3; % dendritic membrane capacitance
-C_s = 2*pi*r_s*l_s*C_m*10^3; % somatic membrane capacitance
-
-a = 34636*10^-4;
 %% initilization
 V = -65;
 
@@ -63,30 +38,20 @@ I_syn_s = zeros(1,length(time));
 I_syn_d = zeros(1,length(time));
 
 %% 
-I_inj_s(2*Fs:3*Fs) = 10;
+I_inj_s(0.5*Fs:end) = 0;
 %% simulation
-for t = 1:length(time)
-      
-    I_Na = g_Na_max*m^3*h*(V-E_Na);
-    I_Kf = g_Kf_max*n^4*(V-E_K);
-    I_Ks = g_Ks_max*q*(V-E_K);
-    
-    dV = (-g_l*(V-E_l) - I_Na - I_Kf - I_Ks + I_inj_s(t)/a)/C_m;    
-    V = 1/Fs*dV + V;
-    
-    alpha_m = 0.32*(13.1-V-58)/(exp((13.1-V-58)/4)-1);
-    beta_m = 0.28*(V-40.1-58)/(exp((V-40.1-58)/5)-1);
-    alpha_h = 0.128*exp((17-V-68)/18);
-    beta_h = 4/(1+exp((40-V-68)/5));
-    alpha_n = 0.016*(35.1-V-58)/(exp((35.1-V-58)/5)-1);
-    beta_n = 0.25*exp((20-V-58)/40);
-    alpha_q = min([0.2*10e-4*n,0.01]);
-    beta_q = 0.001;
-    
-    m = state_var(m,alpha_m,beta_m,Fs);
-    h = state_var(h,alpha_h,beta_h,Fs);
-    n = state_var(n,alpha_n,beta_n,Fs);
-    q = state_var(q,alpha_q,beta_q,Fs);
+for t = 1:length(time)   
+%     if t >= 0.5*Fs
+%         V = 100;
+%     else
+%         V = 0;
+%     end
+    V = f_V(V,m,h,n,q,I_inj_s(t),Fs);
+        
+    m = f_m(m,V,Fs);
+    h = f_h(h,V,Fs);
+    n = f_n(n,V,Fs);
+    q = f_q(q,V,Fs);
     
     V_vec(t) = V;
  
@@ -102,9 +67,124 @@ plot(time,I_inj_s)
 subplot(2,1,2)
 plot(time,V_vec)
 
+figure(2)
+plot(time,m_vec)
+hold on 
+plot(time,h_vec)
+plot(time,n_vec)
+plot(time,q_vec)
+legend('m','h','n','q')
 
 
-function x = state_var(x,alpha,beta,Fs)
-    dx = alpha*(1-x) - beta*x;
-    x = 1/Fs*dx + x;
+function x = f_m(x,V,Fs)
+    step = 1/Fs;
+    V_T = -58;
+    alpha = -0.32*(V-V_T-13)/(exp(-(V-V_T-13)/4)-1)*1000;
+    beta = 0.28*(V-V_T-40)/(exp((V-V_T-40)/5)-1)*1000;
+    
+    k_1 = f_dx(x,alpha,beta);
+    y_1 = x + k_1*step/2;
+    k_2 = f_dx(y_1,alpha,beta);
+    y_2 = x + k_2*step/2;
+    k_3 = f_dx(y_2,alpha,beta);
+    y_3 = x + k_3*step/2;
+    k_4 = f_dx(y_3,alpha,beta);
+    x = x + step/6*(k_1+2*k_2+2*k_3+k_4);
+    
+    function dx = f_dx(x,alpha,beta)
+        dx = alpha*(1-x) - beta*x;
+    end
+end
+
+function x = f_h(x,V,Fs)
+    step = 1/Fs;
+    V_T = -58;
+    V_S = -10;
+    alpha = 0.128*exp(-(V-V_T-V_S-17)/18)*1000;
+    beta = 4/(1+exp(-(V-V_T-V_S-40)/5))*1000;
+    
+    k_1 = f_dx(x,alpha,beta);
+    y_1 = x + k_1*step/2;
+    k_2 = f_dx(y_1,alpha,beta);
+    y_2 = x + k_2*step/2;
+    k_3 = f_dx(y_2,alpha,beta);
+    y_3 = x + k_3*step/2;
+    k_4 = f_dx(y_3,alpha,beta);
+    x = x + step/6*(k_1+2*k_2+2*k_3+k_4);
+    
+    function dx = f_dx(x,alpha,beta)
+        dx = alpha*(1-x) - beta*x;
+    end
+end
+
+function x = f_n(x,V,Fs)
+    step = 1/Fs;
+    V_T = -58;
+    alpha = -0.032*(V-V_T-15)/(exp(-(V-V_T-15)/5)-1)*1000;
+    beta = 0.5*exp(-(V-V_T-10)/40)*1000;
+    
+    k_1 = f_dx(x,alpha,beta);
+    y_1 = x + k_1*step/2;
+    k_2 = f_dx(y_1,alpha,beta);
+    y_2 = x + k_2*step/2;
+    k_3 = f_dx(y_2,alpha,beta);
+    y_3 = x + k_3*step/2;
+    k_4 = f_dx(y_3,alpha,beta);
+    x = x + step/6*(k_1+2*k_2+2*k_3+k_4);
+    
+    function dx = f_dx(x,alpha,beta)
+        dx = alpha*(1-x) - beta*x;
+    end
+end
+
+function x = f_q(x,V,Fs)
+    step = 1/Fs;
+    
+    alpha = 0.0001*(V+30)/(1-exp(-(V+30)/9))*1000;
+    beta = -0.0001*(V+30)/(1-exp((V+30)/9))*1000;
+    
+    k_1 = f_dx(x,alpha,beta);
+    y_1 = x + k_1*step/2;
+    k_2 = f_dx(y_1,alpha,beta);
+    y_2 = x + k_2*step/2;
+    k_3 = f_dx(y_2,alpha,beta);
+    y_3 = x + k_3*step/2;
+    k_4 = f_dx(y_3,alpha,beta);
+    x = x + step/6*(k_1+2*k_2+2*k_3+k_4);
+    
+    function dx = f_dx(x,alpha,beta)
+        dx = alpha*(1-x) - beta*x;
+    end
+end
+
+function x = f_V(x,m,h,n,q,I_input,Fs)
+    step = 1/Fs;
+    k_1 = f_dV(x,m,h,n,q,I_input);
+    y_1 = x + k_1*step/2;
+    k_2 = f_dV(y_1,m,h,n,q,I_input);
+    y_2 = x + k_2*step/2;
+    k_3 = f_dV(y_2,m,h,n,q,I_input);
+    y_3 = x + k_3*step/2;
+    k_4 = f_dV(y_3,m,h,n,q,I_input);
+    x = x + step/6*(k_1+2*k_2+2*k_3+k_4);
+    
+    function dx = f_dV(x,m,h,n,q,I_input)
+        a = 34636*10^-4;
+
+        C_m  = 1; % specific membrain capacitance (microF/cm^2)
+        E_Na = 115;
+        E_K = -15;
+        E_l = -80;
+        
+        g_l = 0.045; %(mS/cm^2)
+        g_Na_max = 30; %*2*pi*r_s*l_s; %(mS/cm^2)
+        g_Kf_max = 4;% *2*pi*r_s*l_s; %(mS/cm^2)
+        g_Ks_max = 16; %*2*pi*r_s*l_s; %(mS/cm^2)
+
+        I_Na = g_Na_max*m^3*h*(x-E_Na);
+        I_Kf = g_Kf_max*n^4*(x-E_K);
+        I_Ks = g_Ks_max*q*(x-E_K);
+    
+        dx = (-g_l*(x-E_l) - I_Na - I_Kf - I_Ks + I_input)/C_m*1000;   
+    end
 end
