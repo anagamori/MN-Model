9bfa6ae93eaae66f3433a9bb9725f4983e6b5c71
Akira <nagamori@usc.edu>
2019-08-14 10:00:12 -0700

    remove files

--- nrn-7.6/src/mswin/lib/neuron2.sh
+++
@@ -1,6 +0,0 @@
-if ! $* ; then
-echo '
-NEURON exited abnormally. Press the return key to close this window
-'
-read a
-fi
--- nrn-7.6/src/nrnoc/nrnversion.h
+++
@@ -1,4 +0,0 @@
-#define GIT_DATE "2019-04-19"
-#define GIT_BRANCH "HEAD"
-#define GIT_CHANGESET "603da174"
-#define GIT_DESCRIBE "7.6.7"
