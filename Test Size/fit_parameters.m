477a1d058c79901e746af536592473c822c0d565
Akira <nagamori@usc.edu>
2019-09-12 15:40:53 -0700

    update

--- Test Size/fit_parameters.m
+++ Test Size/fit_parameters.m
@@ -17,6 +17,11 @@ testMN = loc;
 
 MDR = modelParameter.MDR(testMN);
 PDR = modelParameter.PDR(testMN);
+
+I_max = 100;
+I_th = 100*val;
+slope_target = (PDR-MDR)/(I_max-I_th);
+
 %%
 type = 'S';
 nMN = '1';
@@ -25,47 +30,60 @@ cd(data_folder)
 load([type '_' nMN])
 cd(code_folder)
 
-%%
-amp_vec = 15:70;
-for i = 1:length(amp_vec)
-    i
-    Fs = 10000;
-    time = 0:1/Fs:5;
-    noise_amp = 0;
-    
-    amp = amp_vec(i);
-    input  = zeros(1,length(time));
-    input(1*Fs+1:end) = amp;
-    
-    inputOpt = 1;
-    pltOpt = 0;
-    %%
-    [binary,V_s,V_d] = Cisi2008_function(param,time,input,Fs,noise_amp,inputOpt,pltOpt);
-    
-    spike_time = find(binary(end-2*Fs+1:end));
-    ISI = diff(spike_time)/(Fs/1000);
-    mean_FR(i) = mean(1./ISI*1000);
-    CoV_FR(i) = std(1./ISI*1000)/mean_FR(i)*100;
-end
-%%
-index_t = find(~isnan(mean_FR));
-area_s = 2*pi*param.l_s*param.r_s;
-current = amp_vec*area_s*10^3;
+count = 1;
 
-current_th = current(index_t(1))
-min_DR = mean_FR(index_t(1))
+error = 1;
+
+while abs(error) < 0.01
+    %%
+    amp_vec = 0:1:100;
+    mean_FR = zeros(1,length(amp_vec));
+    CoV_FR = zeros(1,length(amp_vec));
+    if count == 1
+        for i = 1:length(amp_vec)
+            
+            Fs = 10000;
+            time = 0:1/Fs:5;
+            noise_amp = 0;
+            
+            amp = amp_vec(i);
+            input  = zeros(1,length(time));
+            input(1*Fs+1:end) = amp;
+            
+            inputOpt = 1;
+            pltOpt = 0;
+            %%
+            [binary,V_s,V_d] = Cisi2008_function(param,time,input,Fs,noise_amp,inputOpt,pltOpt);
+            
+            spike_time = find(binary(end-2*Fs+1:end));
+            ISI = diff(spike_time)/(Fs/1000);
+            mean_FR(i) = mean(1./ISI*1000);
+            CoV_FR(i) = std(1./ISI*1000)/mean_FR(i)*100;
+        end
+
+    else
+        
+    end
+    
+    %%
+    index_t = find(~isnan(mean_FR));
+    
+    amp_th = amp_vec(index_t(1));
+    min_DR = mean_FR(index_t(1));
+    
+    p_fit = polyfit(amp_vec(index_t(10):end),mean_FR(index_t(10):end),1);
+    y1 = polyval(p_fit,amp_vec(index_t));
+    
+    error = slope_target - p_fit(1);
+    if error > 0
+        param.g_Ks = param.g_Ks + 0.01;
+    else
+        param.g_Ks = param.g_Ks + 0.01;
+    end
+    count = count + 1;
+end
 
 figure(1)
-plot(current,mean_FR,'LineWidth',2)
-hold on
-plot([min(current) max(current)],[MDR MDR],'--k','LineWidth',1)
-plot([min(current) max(current)],[PDR PDR],'--k','LineWidth',1)
-xlabel('Injected Current (nA)','FontSize',14)
-ylabel('Dischage Rate (Hz)','FontSize',14)
-set(gca,'TickDir','out');
-set(gca,'box','off')
-
-figure(2)
 plot(amp_vec,mean_FR,'LineWidth',2)
 hold on
 plot([min(amp_vec) max(amp_vec)],[MDR MDR],'--k','LineWidth',1)
