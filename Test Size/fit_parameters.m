ccbf52b63703821a293325b77b7d69398b669806
Akira <nagamori@usc.edu>
2019-09-12 16:07:09 -0700

    update

--- Test Size/fit_parameters.m
+++ Test Size/fit_parameters.m
@@ -33,37 +33,35 @@ cd(code_folder)
 count = 1;
 
 error = 1;
-perturbation_amp = 0.2;
-for j = 1:10 %while abs(error) > 0.01
+perturbation_amp = 0.3;
+for j = 1:5 %while abs(error) > 0.01
+    j
     %%
     amp_vec = 0:1:100;
     mean_FR = zeros(1,length(amp_vec));
     CoV_FR = zeros(1,length(amp_vec));
-    if count == 1
-        for i = 1:length(amp_vec)
-            
-            Fs = 10000;
-            time = 0:1/Fs:5;
-            noise_amp = 0;
-            
-            amp = amp_vec(i);
-            input  = zeros(1,length(time));
-            input(1*Fs+1:end) = amp;
-            
-            inputOpt = 1;
-            pltOpt = 0;
-            %%
-            [binary,V_s,V_d] = Cisi2008_function(param,time,input,Fs,noise_amp,inputOpt,pltOpt);
-            
-            spike_time = find(binary(end-2*Fs+1:end));
-            ISI = diff(spike_time)/(Fs/1000);
-            mean_FR(i) = mean(1./ISI*1000);
-            CoV_FR(i) = std(1./ISI*1000)/mean_FR(i)*100;
-        end
-
-    else
+    
+    for i = 1:length(amp_vec)
         
+        Fs = 10000;
+        time = 0:1/Fs:5;
+        noise_amp = 0;
+        
+        amp = amp_vec(i);
+        input  = zeros(1,length(time));
+        input(1*Fs+1:end) = amp;
+        
+        inputOpt = 1;
+        pltOpt = 0;
+        %%
+        [binary,V_s,V_d] = Cisi2008_function(param,time,input,Fs,noise_amp,inputOpt,pltOpt);
+        
+        spike_time = find(binary(end-2*Fs+1:end));
+        ISI = diff(spike_time)/(Fs/1000);
+        mean_FR(i) = mean(1./ISI*1000);
+        CoV_FR(i) = std(1./ISI*1000)/mean_FR(i)*100;
     end
+    
     
     %%
     index_t = find(~isnan(mean_FR));
@@ -71,28 +69,29 @@ for j = 1:10 %while abs(error) > 0.01
     amp_th = amp_vec(index_t(1));
     min_DR = mean_FR(index_t(1));
     
-    p_fit = polyfit(amp_vec(index_t(10):end),mean_FR(index_t(10):end),1);
+    p_fit = polyfit(amp_vec(index_t),mean_FR(index_t),1);
     y1 = polyval(p_fit,amp_vec(index_t));
     
     error = slope_target - p_fit(1);
-    %x(9) +  (x(9)*perturbation_amp)./2.^(k-2)
+    
     if error > 0
-        param.g_Ks = param.g_Ks + (param.g_Ks*perturbation_amp)./2.^(count-2);
-    else
         param.g_Ks = param.g_Ks - (param.g_Ks*perturbation_amp)./2.^(count-2);
+    else
+        param.g_Ks = param.g_Ks + (param.g_Ks*perturbation_amp)./2.^(count-2);
     end
     count = count + 1;
     
     figure(1)
     plot(amp_vec,mean_FR,'LineWidth',2)
     hold on
+    plot(amp_vec(index_t),y1,'--k','LineWidth',1)
     plot([min(amp_vec) max(amp_vec)],[MDR MDR],'--k','LineWidth',1)
     plot([min(amp_vec) max(amp_vec)],[PDR PDR],'--k','LineWidth',1)
     xlabel('Injected Current (nA)','FontSize',14)
     ylabel('Dischage Rate (Hz)','FontSize',14)
     set(gca,'TickDir','out');
     set(gca,'box','off')
-
+    
 end
 
 
